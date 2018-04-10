bc09206edd2cf74fa5f13918100fee3e15bc57aa
Andrii Sviatogor <av.sviatogor@gmail.com>
2018-04-10 23:07:09 +0300

    add cycle page

---
+++ js/cycles.js
@@ -0,0 +1,12 @@
+new Vue({
+    el: '#app',
+    data: {
+        show: true,
+        cars: [
+            {model: "BMW", speed: 205.8},
+            {model: "Audi", speed: 240.21},
+            {model: "AMG", speed: 350.4},
+            {model: "Ford", speed: 160.5},
+        ]
+        }
+    });
